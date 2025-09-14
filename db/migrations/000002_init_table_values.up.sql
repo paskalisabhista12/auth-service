03157775ba3d2d8448a98266553a3cff84b28181
Paskalis Abhista Bagaskara Yustiyanto <paskalis226@gmail.com>
2025-09-14 13:04:38 +0700

    feat: Update docker-yaml env configuration, nginx configm and add new default value on table permission

--- db/migrations/000002_init_table_values.up.sql
+++ db/migrations/000002_init_table_values.up.sql
@@ -3,3 +3,7 @@ VALUES
   ('SUPERADMIN', 'Super administrator with full system access'),
   ('ADMIN', 'Administrator with elevated privileges'),
   ('USER', 'Standard user with limited access');
+
+INSERT INTO public.permissions (name, description)
+VALUES
+    ('ALL', 'Permission to bypass all endpoint');
